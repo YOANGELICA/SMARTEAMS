749ac57d7f470a0e931a51e8c400a74d2622bbb8
YOANGELICA <maria_a.portocarrero@uao.edu.co>
2022-09-24 23:27:41 -0500

    demaga ge gi go gu

--- src/components/profile-page/UserInfo.vue
+++ src/components/profile-page/UserInfo.vue
@@ -1,15 +1,21 @@
 <template>
-    <div>
-        ASDDASFSD
+    <div class="main-container">
+        <div class="right-container" style="width: 25%; height: 100%;float:left; ">
+            <img src="@/assets/icon-image.png" style="height: 100px; width:100px">
+        </div>
+        <div class="left-container" style="width: 75%; height: 100%; float:right; ">
+            <h2 style=" font-weight: 750"> {{userName}} Angelica Portocarrero Quintero</h2>
+            <p> {{UserEmail}} angelica.portocarrero12+1@gmail.com</p>
+        </div>
     </div>
 </template>
 
 <script></script>
 
 <style scoped>
-    div{
-        height: 130px;
-        width: 380px;
+    .main-container {
+        height: 150px;
+        width: 470px;
         background-color: #EBE9E9;
         border-radius: 24px;
         padding: 20px;
